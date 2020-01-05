e93bcf95573893eff9f700a5a25f31f596585973
deancn <dean@airdb.com>
2020-01-06 02:25:53 +0800

    add wiki

--- cmd/wiki.go
+++ cmd/wiki.go
@@ -2,8 +2,12 @@ package cmd
 
 import (
 	"bytes"
+	"encoding/json"
 	"fmt"
+	"github.com/imroc/req"
+	"log"
 	"os/exec"
+	"sort"
 	"strings"
 
 	"github.com/spf13/cobra"
@@ -26,17 +30,41 @@ func Usage() {
 	fmt.Println("Usage:")
 	fmt.Println("  adb wiki [project_name]")
 	fmt.Println()
-	fmt.Println("Projects:")
-	fmt.Println("\tdev")
-	fmt.Println("\twork")
-	fmt.Println("\tgo")
-	fmt.Println("\tinterview")
-	fmt.Println("\tai")
-	fmt.Println("\tkube")
-	fmt.Println("\tbbhj")
-	fmt.Println("\tlinux")
+	listRepos()
 	fmt.Println()
-	fmt.Println("Airdb Wiki: https://airdb.wiki")
+	fmt.Println("Airdb Wiki: https://airdb.wiki.")
+}
+
+type Repo struct {
+	ID uint `json:"id"`
+	NodeID string `json:"node_id"`
+	Name string `json:"name"`
+}
+
+func listRepos() {
+	apiurl := "https://api.github.com/orgs/airdb-wiki/repos"
+	resp, err := req.Get(apiurl)
+	if err != nil {
+		log.Println("Query Github failed. https://github.com/airdb/airdb-wiki")
+		return
+	}
+
+	repos := make([]Repo, 0)
+	err = json.Unmarshal(resp.Bytes(), &repos)
+	if err != nil {
+		log.Println("json unmarshall failed.")
+		return
+	}
+
+	sort.Slice(repos, func(i, j int) bool { return len(repos[i].Name) < len(repos[j].Name) })
+	fmt.Println("Projects:")
+	for _, repo := range repos {
+		switch repo.Name {
+		case "airdb-wiki.github.io":
+		default:
+			fmt.Printf("\t%s%30s\n", repo.Name, "thttps://airdb.wiki/" + repo.Name)
+		}
+	}
 }
 
 func wiki(wikiName string) {
