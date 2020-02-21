3c95eab603a7006f32a072de74ec4e56750d764e
deancn <dean@airdb.com>
2020-02-21 13:35:46 +0800

    update

--- cmd/root.go
+++ cmd/root.go
@@ -25,6 +25,7 @@ func init() {
 	rootCmd.AddCommand(releaseCommand)
 	rootCmd.AddCommand(mysqlCommand)
 	rootCmd.AddCommand(updateCommand)
+	rootCmd.AddCommand(completionBashCommand)
 	rootCmd.AddCommand(manCommand)
 	manCommand.AddCommand(gitInitCommand)
 	manCommand.AddCommand(dockerInitCommand)
@@ -44,4 +45,27 @@ func Execute() {
 		fmt.Println(err)
 		os.Exit(1)
 	}
+}
+
+// completionCmd represents the completion command
+var completionBashCommand = &cobra.Command{
+	Use:   "completion",
+	Short: "Generates bash completion scripts",
+	Long: `To load completion run
+
+. <(bitbucket completion)
+
+To configure your bash shell to load completions for each session add to your bashrc
+
+# MacOS:
+# adb completion >/usr/local/etc/bash_completion.d/adb
+# ~/.bashrc or ~/.profile
+. <(bitbucket completion)
+`,
+	Run: func(cmd *cobra.Command, args []string) {
+		err := rootCmd.GenBashCompletion(os.Stdout)
+		if err != nil {
+			fmt.Println("Generates bash completion scripts failed!")
+		}
+	},
 }
--- cmd/scaffold_test.go
+++ cmd/scaffold_test.go
@@ -11,8 +11,6 @@ import (
 )
 
 func TestScaffold(t *testing.T) {
-
-	t.Log("xxx")
 	// 1tempDir, err := ioutil.TempDir(filepath.Join(Gopath, "src"), "test")
 	tempDir, err := ioutil.TempDir(filepath.Join("/tmp", "src"), "test")
 
