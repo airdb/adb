fae34426fea3570c3b48b6c7a56853dc2ddd868a
Dean <dean@airdb.com>
2020-07-22 10:13:33 +0800

    skip dirs

--- cmd/manual.go
+++ cmd/manual.go
@@ -190,8 +190,8 @@ var opensslCommand = &cobra.Command{
 		fmt.Println("")
 		fmt.Println("\topenssl  x509 -text -in ssl.chain.crt")
 		fmt.Println("")
-		fmt.Println("\topenssl s_client -servername www.airdb.com  -connect www.airdb.com:443 </dev/null 2>/dev/null")
-		fmt.Println("\tcert -f md  www.airdb.com")
+		fmt.Println("\topenssl s_client -servername www.airdb.com -connect www.airdb.com:443 </dev/null 2>/dev/null")
+		fmt.Println("\tcert -f md www.airdb.com")
 		fmt.Println("")
 		fmt.Println("\tRefer: https://github.com/genkiroid/cert")
 	},
