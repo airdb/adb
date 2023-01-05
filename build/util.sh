15b915b1972556c26bb81c6c42d9690dddfeb6af
deancn <dean@airdb.com>
2023-01-05 14:58:38 +0800

    add lego

--- build/util.sh
+++ build/util.sh
@@ -6,9 +6,9 @@ BUILD=$(git rev-parse HEAD)
 BUILD_TS=$(date +%s)
 
 LDFLAGS="-s -w \
-			-X=github.com/airdb/adb/internal/adblib.Version=$VERSION \
-            -X=github.com/airdb/adb/internal/adblib.Build=$BUILD \
-            -X=github.com/airdb/adb/internal/adblib.BuildTime=$BUILD_TS"
+	-X=github.com/airdb/adb/internal/adblib.Version=$VERSION \
+	-X=github.com/airdb/adb/internal/adblib.Build=$BUILD \
+	-X=github.com/airdb/adb/internal/adblib.BuildTime=$BUILD_TS"
 
 function until::build() {
 	CGO_ENABLED=0 GOOS=linux GOARCH=amd64 go build -ldflags "$LDFLAGS" -o ./output/adb main.go
